e8d0d0ae35efea89afa0a130bd2ed2f91efa8c67
davidbillamboz <david@chugulu.com>
2011-12-21 15:40:53 +0100

    fix deviceready listener

--- www/app/main.js
+++ www/app/main.js
@@ -9,6 +9,7 @@ function Application() {
 
   self.ready = null;
   self.phonegap = null;
+  self.initialized = false;
   
   self.container = null;
 
@@ -17,17 +18,73 @@ function Application() {
 
 Application.prototype.wait = function() {
   var self = this;
- 
-  document.addEventListener("deviceready", function() { self.phonegap = true; self.initialize(); }, false); // phonegap ready
-  $(function() { self.ready = true;  self.initialize(); }); // dom ready
+  
+  console.log("waiting for dom and phonegap initialization");
 
-  return this;
+  $(window).load(function() {
+    if(typeof PhoneGap !== "undefined" && typeof PhoneGap.onDeviceReady != "undefined" && PhoneGap.onDeviceReady.fired === true) 
+      self.onDeviceReady();
+    else document.addEventListener("deviceready", function() { self.onDeviceReady(); }, false);
+  });
+
+  $(function() { self.onDomReady(); }); // dom ready
+
+  return self;
+};
+
+/*
+The Android PhoneGap javascript version cancel the deviceready listeners on initialization.
+It appears to be more logical to listen before the instanciation...
+So we loop until we deviceready has fired.
+*/
+Application.prototype.waitForDeviceReady = function(callback) {
+  var self = this;
+
+  // don't need a timer, it's already ready
+  if(!self.phonegap && typeof PhoneGap !== "undefined" && PhoneGap.onDeviceReady.fired) {
+    callback();
+
+  // not ready yet, let's wait  
+  } else if(!self.phonegap) {
+    var loopsMax = 1000,
+        loopCurrent = 0,
+        loopFrequence = 1,
+        timer = setInterval(function() {
+
+      if(typeof PhoneGap !== "undefined" && PhoneGap.onDeviceReady.fired) {
+        console.log("deviceready fired in " + (loopCurrent * loopFrequence) + "ms");
+        clearInterval(timer);
+        callback();
+        return;
+      }
+      loopCurrent++;
+      if(loopCurrent == loopsMax) {
+        console.error("deviceready didn't fired in " + (loopCurrent * loopFrequence) + "ms");
+        clearInterval(timer);
+      }
+    }, loopFrequence);         
+  }
+};
+
+Application.prototype.onDeviceReady = function() {
+  var self = this;
+  
+  self.phonegap = true; 
+  self.initialize();
+};
+
+Application.prototype.onDomReady = function() {
+  var self = this;
+  
+  self.ready = true; 
+  self.initialize();
 };
 
 Application.prototype.initialize = function() {
   var self = this;
 
   if(self.ready && self.phonegap) {
+    self.initialized = true;
     console.log("application initialized");
 
     self.container = $("#main");
