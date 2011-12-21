91569504fd65cae8cdbea7e71cc5075770f9e146
davidbillamboz <david@chugulu.com>
2011-12-21 15:41:13 +0100

    build 1.3.0

--- android/bin/PhoneGapInAppBilling-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/davidbillamboz/Projects/PhoneGapInAppBilling/android/bin/PhoneGapInAppBilling-debug-unaligned.apk : \
-/Users/davidbillamboz/Projects/PhoneGapInAppBilling/android/bin/PhoneGapInAppBilling.ap_ \
-/Users/davidbillamboz/Projects/PhoneGapInAppBilling/android/bin/classes.dex \
-/Users/davidbillamboz/Projects/PhoneGapInAppBilling/android/src/com/phonegap/phonegapinappbilling/PhoneGapInAppBilling.java-e \
-/Users/davidbillamboz/Projects/PhoneGapInAppBilling/android/libs/phonegap-1.2.0.jar \
